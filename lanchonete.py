3bcbe3363a7b770d3014ff288973c284d9c33d2a
suzaneemely22-cmyk <suzaneemely22@gmail.com>
2026-09-23 20:20:39 -0300

    Update lanchonete.py

--- lanchonete.py
+++ lanchonete.py
@@ -1,1 +1,9 @@
+print("================================")
+print("     LANCHONETE SABOR & CIA")
+print("================================")
 
+nome = input("Digite seu nome: ")
+
+print()
+print("Olá,", nome, "!")
+print("Seja bem-vindo(a) à nossa lanchonete!")
